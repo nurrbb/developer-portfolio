445e63c30fba9e37d12e27763d3c465387bd7b89
nurrbb <enurb320@gmail.com>
2025-07-02 22:02:52 +0300

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Nur B.',
   icons: {
     icon: '/favicon.ico',
   },
